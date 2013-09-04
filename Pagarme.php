577f745f3dc21f93d1075d58a0781c02b70c8699
Henrique Dubugras <hdubugras@gmail.com>
2013-09-04 20:54:59 -0300

    Implement postback class

---
+++ Pagarme.php
@@ -0,0 +1,28 @@
+<?php
+
+if (!function_exists('curl_init')) {
+	throw new Exception('PagarMe needs the CURL PHP extension.');
+}
+if (!function_exists('json_decode')) {
+  throw new Exception('PagarMe needs the JSON PHP extension.');
+}
+
+
+require(dirname(__FILE__) . '/lib/Pagarme/Pagarme.php');
+require(dirname(__FILE__) . '/lib/Pagarme/Exception.php');
+require(dirname(__FILE__) . '/lib/Pagarme/TransactionException.php');
+require(dirname(__FILE__) . '/lib/Pagarme/ApiException.php');
+require(dirname(__FILE__) . '/lib/Pagarme/RestClient.php');
+require(dirname(__FILE__) . '/lib/Pagarme/Request.php');
+require(dirname(__FILE__) . '/lib/Pagarme/Model.php');
+require(dirname(__FILE__) . '/lib/Pagarme/TransactionCommon.php');
+require(dirname(__FILE__) . '/lib/Pagarme/Transaction.php');
+require(dirname(__FILE__). '/lib/Pagarme/Plan.php');
+require(dirname(__FILE__) . '/lib/Pagarme/Subscription.php');
+require(dirname(__FILE__) . '/lib/Pagarme/Postback.php');
+
+
+
+
+
+?>
