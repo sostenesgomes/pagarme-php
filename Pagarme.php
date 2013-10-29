595d2dd4f536b306da0974ac78ff8418dd43b39d
Sóstenes Gomes <analista.sn@gmail.com>
2013-10-29 16:34:47 -0200

    Update Pagarme.php
    Simplificando a verificação da chamada de classes com ou sem prefixos 'PagarMe_'.

--- Pagarme.php
+++ Pagarme.php
@@ -12,21 +12,11 @@ function __autoload($class){
 
 	$dir = dirname(__FILE__) . DIRECTORY_SEPARATOR . "lib" . DIRECTORY_SEPARATOR . "Pagarme" . DIRECTORY_SEPARATOR;
 	
-	$file = $dir . $class . ".php";
-		
+	$file = $dir . ((strstr($class, "PagarMe_")) ? str_replace("PagarMe_", "", $class) : $class) . ".php";
+
 	if (file_exists($file)){
 		require_once($file);
 		return;
-	}elseif (strstr($class, "PagarMe_")) {
-		$file = $dir . str_replace("PagarMe_", "", $class) . ".php";
-		
-		if (file_exists($file)){
-			require_once($file);
-			return;		
-		}else{
-			throw new Exception("Unable to load" .$class);
-		}
-
 	}else{
 		throw new Exception("Unable to load" .$class);
 	}
